a0c439de02220acf6eac76499f4bf1907bb9ec1f
Toby <toby@tobsd.org>
2024-12-24 05:29:59 -0500

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# so far I've installed: git, curl, wget, vim
+# I need to install a GUI
+# X can be installed via ports or via pkg
+# Lets see what I need to do in order to include nvidia drivers
